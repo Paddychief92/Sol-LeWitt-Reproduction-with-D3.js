db62e77f66bde1e9f54be5a79e70ddc7a4619006
paddy meade <patrickmeade4@gmail.com>
2023-03-17 22:19:31 +0000

    add for loop for elements and svg element

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ const height = window.innerHeight;
 const svg = d3.select('body').append('svg');
 svg.attr('width', width);
 svg.attr('height', height);
+
+const n = 100;
+const marks = [];
+for (let i = 0; i < n; i++) {
+    marks.push({
+        y: i * 20,
+        width: width,
+        height: 10,
+        mask: 'url(#circle-mask',
+    });
+}
+
+svg
+.selectAll('rect')
+.data(marks)
+.join('rect')
+.attr('y', (d) => d.y)
+.attr('width', (d) => d.width)
+.attr('height', (d) => d.height)
+.attr('mask', (d) => d.mask)
