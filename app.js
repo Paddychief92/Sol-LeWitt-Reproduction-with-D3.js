17c8bb5e49e8bf3d9d33638111a07b507a228b12
paddy meade <patrickmeade4@gmail.com>
2023-03-18 02:56:44 +0000

    Add circle mask

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ svg
 .data(d3.range(n))
 .join('rect')
 .attr('y', (d) => d * 20)
-.attr('width', (d) => d.width)
-.attr('height', (d) => d.height)
-.attr('mask', (d) => d.mask)
+.attr('width', width)
+.attr('height', 10)
+.attr('mask', 'url(#circle-mask)');
+
+const mask = svg.append('mask').attr('id', 'circle-mask');
+
+mask.append('rect').attr('width', width).attr('height', height).attr('fill', 'black');
+
+mask.append('circle').attr('cx', width / 2)
+.attr('cy', height / 2).attr('r', 200).attr('fill', 'white');
